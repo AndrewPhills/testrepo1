bab6c9b45dc222f6c53c0b68a03c8b8d8ddf03de
AndrewPhills <andrei97filip@gmail.com>
2019-09-16 10:16:32 +0300

    gyjyjtyu

---
+++ Abstract_Polymorphism/test/Tutorial.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{  public abstract class Abstracta
+    {
+        public abstract void MyMetod() ;  // abstract methods in abstract classes cannot have a body
+
+
+        public void MynewMethod() //void methods ( -no need to return- ) can have body and implement 
+        {
+
+        }
+
+    }
+
+
+    public class Abstract1 : Abstracta //abstract implements method here
+    {
+        public override void MyMetod()
+        {
+            base.MynewMethod();
+        }
+    }
+    internal interface Guru99Interface  // Define the public interface
+    { 
+        //here we define the methods
+        //no more acces modifiers in interface
+
+        void SetTutorial(int pID, string pName);
+        String GetTutorial();
+        void MyMethod();
+
+    }
+
+    public class Guru99Tutorial : Guru99Interface // class extends the interface
+    {
+        protected int TutorialID;
+        protected string TutorialName;
+
+        public void SetTutorial(int pID, String pName)
+        {
+            TutorialID = pID;
+            TutorialName = pName;
+        }
+        public String GetTutorial()
+        {
+            return TutorialName;
+        }
+
+
+        public virtual void MyMethod()
+        {
+            Console.WriteLine("New method");
+
+        }
+
+    
+
+    
+
+        static void Main(string[] args)
+        {
+            Guru99Interface pTutor = new Guru99Tutorial();
+
+           
+            pTutor.MyMethod(); 
+         
+            Guru99Interface item = new MyClass2();
+            item.MyMethod();
+            pTutor.MyMethod();
+            item.MyMethod();
+
+            Console.ReadKey();
+        }
+    } 
+
+    public class MyClass2 : Guru99Tutorial
+    {
+        public override void MyMethod()
+        {
+            base.MyMethod();
+            Console.WriteLine("Another one");
+        } 
+    }
+}
---
+++ Serialized.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test2
+{
+
+    [Serializable]  //Serializable attribute
+
+    public class Tutorial  //class to serialize
+    {
+        public int ID;
+        public String Name;
+
+
+        static void Main(string[] args)
+        {
+            Tutorial obj = new Tutorial(); //Object (obj) datatype class tutorial instance
+            obj.ID = 1;
+            obj.Name = ".Net";  // INITIALIZE AN OBJECT
+
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(@"E:\ExampleNew.txt", FileMode.Create, FileAccess.Write);////////////
+            // CREATE THE FILE STREAM
+
+            formatter.Serialize(stream, obj);
+            stream.Close();  // SERIALIZE THE OJECT
+
+            stream = new FileStream(@"E:\ExampleNew.txt", FileMode.Open, FileAccess.Read);
+            //We create the object "stream" to open the file Example.txt in reading only mode.
+
+            Tutorial objnew = (Tutorial)formatter.Deserialize(stream);
+            // CREATE THE FILE STREAM
+            //DESERIALIZE THE OBJECT
+
+            Console.WriteLine(objnew.ID);
+            Console.WriteLine(objnew.Name);
+            // WRITE THE DATA TO THE CONSOLE
+
+            Console.ReadKey();
+        }
+    }
+} 
